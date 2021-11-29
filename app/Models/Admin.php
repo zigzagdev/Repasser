c25d63658ed50dca6c12006352aec3b51d08f109
zigzagdev <msts.oo0131@gmail.com>
2021-11-29 16:06:17 +0900

    Admin_EloquentModel_create

---
+++ app/Models/Admin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+   protected $table = 'admins';
+
+   protected $guarded = ['id'];
+   protected $fillable = ['user_name','item_name','description','recommend_flag','item_category'];
+
+}
