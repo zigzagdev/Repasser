058e3fd8d4888cc89ce6e0c96092a0117a211043
zigzagdev <msts.oo0131@gmail.com>
2021-12-04 09:27:15 +0900

    item_migrationfile_add

---
+++ database/migrations/2021_12_04_092500_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('item_name','20')->comment('商品名');
+            $table->string('category_name','30')->comment('カテゴリー分類名');
+            $table->string('recommend_flag')->comment('商品おすすめフラグ')->default('0');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
