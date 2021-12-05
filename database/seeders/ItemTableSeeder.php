b5f41ea1259767a06d27049c67ba6b42dd9bbc13
zigzagdev <msts.oo0131@gmail.com>
2021-12-05 12:04:33 +0900

    Seeder_add_foregin_key

--- database/seeders/ItemTableSeeder.php
+++ database/seeders/ItemTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+
 
 class ItemTableSeeder extends Seeder
 {
@@ -14,12 +16,21 @@ class ItemTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('items')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('admins')->truncate();
+        $this->call([AdminTableSeeder::class]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+
+        $admin = Admin::find(1);
+        for($i=1 ; $i<=5 ; $i++)
 
         $datas = [
             [
                 'item_name'=>'iPhoneX','category_name'=>'周辺機器','item_content'=>'iPhoneXです。','recommend_flag'=>'0',
-                'created_at'=>date('Y-m-d H:i:s'),'updated_at'=>date('Y-m-d H:i:s')
+                'admin_id'=>$admin->id,'created_at'=>date('Y-m-d H:i:s'),'updated_at'=>date('Y-m-d H:i:s')
             ],
         ];
 
