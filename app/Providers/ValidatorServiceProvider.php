054dd2afe441930451fc1d437184c1e340e12a8d
zigzagdev <msts.oo0131@gmail.com>
2021-12-07 20:43:28 +0900

    validation_fixed

--- app/Providers/ValidatorServiceProvider.php
+++ app/Providers/ValidatorServiceProvider.php
@@ -26,8 +26,29 @@ class ValidatorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Validator::extend('',function ($attribute,$value,$parameters,$validator){
-            return preg_match('',$value);
+//        半角英数字のみ（空文字OK）
+        Validator::extend('h_alphanum',function ($attribute,$value,$parameters,$validator){
+            return preg_match('/^[0-9a-zA-Z]*$/',$value);
+        });
+
+//        全角ひらがな（空文字OK）
+        Validator::extend('f_hira',function ($attribute,$value,$parameters,$validator){
+            return preg_match('^[ぁ-んー]*$',$value);
+        });
+
+//        半角英数記号のみ（空文字OK）
+        Validator::extend('h_alpanumnote',function ($attribute,$value,$parameters,$validator){
+            return preg_match('^[a-zA-Z0-9!-/:-@¥[-`{-~]*$',$value);
+        });
+
+//        郵便番号
+        Validator::extend('postal',function ($attribute,$value,$parameters,$validator){
+            return preg_match('\s?[0-9]{3}-?[0-9]{4}',$value);
+        });
+
+//        キャリア端末ハイフンありなし
+        Validator::extend('tel_mobile',function ($attribute,$value,$parameters,$validator){
+            return preg_match('^0[789]0(-\d{4}-\d{4}|\d{8})$',$value);
         });
 
 
@@ -55,18 +76,6 @@ class ValidatorServiceProvider extends ServiceProvider
             return preg_match('',$value);
         });
 
-
-        Validator::extend('',function ($attribute,$value,$parameters,$validator){
-            return preg_match('',$value);
-        });
-
-        Validator::extend('',function ($attribute,$value,$parameters,$validator){
-            return preg_match('',$value);
-        });
-
-        Validator::extend('',function ($attribute,$value,$parameters,$validator){
-            return preg_match('',$value);
-        });
 
         Validator::extend('',function ($attribute,$value,$parameters,$validator){
             return preg_match('',$value);
