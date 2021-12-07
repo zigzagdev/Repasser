091dc141de425a67bf0d0c594bfad9b09858d6c6
zigzagdev <msts.oo0131@gmail.com>
2021-12-07 20:23:51 +0900

    Login_Logout_Header_Footer_add

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    {{--       // config/app.phpの修正を行った。第一引数に.envのapp_nameを渡している。--}}
+    <title>{{ config('app.name',' Repasser | Login ') }}</title>
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+
+<main>
+  <header>
+    @include('layouts/header')
+  </header>
+  <footer>
+    @include('layouts/footer')
+  </footer>
+</main>
