80fb5c3cf9ca92c7bd60474773a06591eed2c36d
zigzagdev <msts.oo0131@gmail.com>
2021-12-20 23:08:12 +0900

    Edit_DeletePage_Add

---
+++ resources/views/item/deedEditItem.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    {{--   // config/app.phpの修正を行った。第一引数に.envのapp_nameを渡している。--}}
+    <title>{{ config('app.name','Repasser') }}</title>
+    <link href="{{asset('css/Item.css')}}" rel="stylesheet">
+    <link href="{{asset('css/admin.css')}}" rel="stylesheet">
+</head>
+<footer>
+    @include('layouts/ItemFooter')
+</footer>
