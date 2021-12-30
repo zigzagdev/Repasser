494fdf74815c7fc786728f3e9ceabeedfc748edb
zigzagdev <msts.oo0131@gmail.com>
2021-12-30 14:04:23 +0900

    button_css_change

--- resources/views/admin/deedAccountShow.blade.php
+++ resources/views/admin/deedAccountShow.blade.php
@@ -4,6 +4,7 @@
 {{--       // config/app.phpの修正を行った。第一引数に.envのapp_nameを渡している。--}}
   <title>{{ config('app.name','Repasser') }}</title>
     <link href="{{asset('css/admin.css')}}" rel="stylesheet">
+    <link href="{{asset('css/item.css')}}" rel="stylesheet">
 </head>
 <body>
   <header>
