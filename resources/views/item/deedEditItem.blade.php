7c87f8cb626d85634a164c659b705755d4e3b5c4
zigzagdev <msts.oo0131@gmail.com>
2022-01-08 17:25:57 +0900

    dd_ItemEdit_Updated

--- resources/views/item/deedEditItem.blade.php
+++ resources/views/item/deedEditItem.blade.php
@@ -30,19 +30,19 @@
           <div class="admin_name">
             <label for="item_name">Item Name</label>
               {{--              因みに、$itemにはControllerで取ってきた配列が入っている。--}}
-            <input type="text" id="item_name" name="item_name" class="name_css" style="width: 320px; height: 30px" value="{{old("item_name.$item->item_name")}}" placeholder="20文字以内">
+            <input type="text" id="item_name" name="item_name" class="name_css" style="width: 320px; height: 30px" value="{{$item->item_name}}" placeholder="20文字以内">
           </div>
           <div class="password_around">
               <label for="admin_password">Item Category</label>
-              <select name="index" style="margin-left: 30px">
-              @foreach(config('category') as $index => $name)
-                <option value="{{ $index }}" {{ old('index') === $index? "selected" : ""}}>{{$name}}</option>
-              @endforeach
+              <select name="item_category" style="margin-left: 30px">
+                @foreach(config('category') as $index => $category )
+                  <option value="{{ $index }}" {{ old('index') === $index? "selected" : ""}}>{{ $category }}</option>
+                @endforeach
               </select>
             </div>
             <div class="email_around">
               <label for="item_content">Product Description</label>
-              <textarea rows="8" cols="40" name="item_content" placeholder="Write some product description">{{old('item_content')}}</textarea>
+              <textarea rows="8" cols="40" name="item_content" placeholder="Write some product description">{{$item->item_content}}</textarea>
             </div>
             <div class="email_around">
               <p style="font-size: 30px; ">Recommendation  or Not</p>
