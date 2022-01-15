07f189f5dd30b991c6c36715d2b46e5ac751a292
zigzagdev <msts.oo0131@gmail.com>
2022-01-15 11:17:22 +0900

    head_add

--- resources/views/ItemDisplay.blade.php
+++ resources/views/ItemDisplay.blade.php
@@ -1,5 +1,7 @@
-
-
-
-
-fuck you
+<!DOCTYPE html>
+<html>
+<head>
+    {{--   // config/app.phpの修正を行った。第一引数に.envのapp_nameを渡している。--}}
+    <title>{{ config('app.name','Repasser') }}</title>
+    <link href="{{asset('css/admin.css')}}" rel="stylesheet">
+</head>
